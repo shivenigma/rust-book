f556634ddfbd9c6c2577e094b9477dd30b571e07
Vignesh M <ms.vignesh31@gmail.com>
2023-07-10 05:28:46 +0530

    moved area to implementation of the struct

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,16 @@ struct Rectangle {
     width: u32,
     height: u32,
 }
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
 fn main() {
     let rect = Rectangle {
         width: 30,
         height: 20,
     };
     println!("The rect is {:#?}", rect);
-    println!("area of rect is {}", rect_area(&rect));
-}
-fn rect_area(rect: &Rectangle) -> u32 {
-    rect.width * rect.height
+    println!("area of rect is {}", rect.area());
 }
